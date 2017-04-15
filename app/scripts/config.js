7d0b317d82b54d19e863625f715ff00dd50bdf8e
Jan Niehusmann <jan@gondor.com>
2017-04-15 12:08:16 +0200

    add config.js to deploy branch

---
+++ app/scripts/config.js
@@ -0,0 +1,42 @@
+// Copy to app/scripts/config.js
+
+window.nodelistconfig = {
+	dataPath: 'https://data.aachen.freifunk.net/',
+	mapPath: 'https://map.aachen.freifunk.net/',
+	siteName: 'Freifunk Regio Aachen',
+	downstreamSource: 'https://github.com/ffac/nodelist/tree/deploy',
+
+	nodeUpStartColor:   {h:  61, s: 100, l: 50},
+	nodeUpEndColor:     {h: 122, s:  97, l: 50},
+	nodeUpGamma:        0.5,
+	nodeUpDays:         30,
+	/// TODO The same for down hosts
+	// nodeDownStartColor: {h:  22, s: 100, l: 82},
+	// nodeDownEndColor:   {h:   0, s: 100, l: 70},
+	
+	dataMagnitudes: [
+		{threshold: Math.pow(1024, 2), suffix: 'KiB', scale: Math.pow(1024, 1), tailing: 3},
+		{threshold: Math.pow(1024, 3), suffix: 'MiB', scale: Math.pow(1024, 2), tailing: 3},
+		{threshold: Math.pow(1024, 4), suffix: 'GiB', scale: Math.pow(1024, 3), tailing: 3},
+		{threshold: Math.pow(1024, 5), suffix: 'TiB', scale: Math.pow(1024, 4), tailing: 3},
+	],
+	
+	showCols: [
+		'hasVpn',
+		'hasLocation',
+		'neighbourCnt',
+		'id',
+		'hostname',
+		'version',
+		'model',
+		'autoupdater',
+		'clients',
+		'gateway',
+		'site',
+		'uptime',
+		'firstSeen',
+		'lastSeen',
+		'rootfsUsage',
+		'memoryUsage',
+	],
+};
